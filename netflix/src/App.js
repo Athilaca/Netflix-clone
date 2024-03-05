2aa38de4b3660b35ae5f25d126099a92fc444c73
Athila <athilaca26@gmail.com>
2024-03-05 15:25:33 +0530

    Netflix UI completed

---
+++ netflix/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./App.css"
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost/>
+    </div>
+  );
+}
+
+export default App;
+
